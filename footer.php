65154f43f330b7dd0981da1103bbd634e204c93c
jorge <husmea-03muescas@icloud.com>
2023-12-26 19:45:35 +0000

    advances

---
+++ footer.php
@@ -0,0 +1,29 @@
+<!DOCTYPE>
+<html lang="en">
+<head>
+    <title>Jorge Castilla's personal web page</title>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="assets/css/myStyle.css">
+</head>
+<body>
+<h3>link to my social media</h3>
+<div id="social-media-flexbox">
+    <div class="social-media">
+        <a href="https://www.linkedin.com/in/jorge-castilla-ruiz-7b1b3b1b2/">
+            <img src="assets/images/linkedin.svg" alt="LinkedIn" width="100" height="100">
+        </a>
+        <p>LinkedIn</p>
+    </div>
+    <div class="social-media">
+        <a href="">
+            <img src="assets/images/x.svg" alt="Twitter" width="100" height="100">
+        </a>
+        <p>Twitter</p>
+    </div>
+    <div class="social-media">
+        <a href="">
+            <img src="assets/images/github.svg" alt="Github" width="100" height="100">
+        </a>
+        <p>Github</p>
+    </div>
+</div>
