51960bc08cc9ed257683c0136f8cbc4e54466b96
jorge <husmea-03muescas@icloud.com>
2023-12-28 20:31:03 +0000

    dark mode+

--- assets/js/buildFooter.js
+++ assets/js/buildFooter.js
@@ -1,15 +1,16 @@
 // Function to build the footer
+
+
+const socialMedias = [
+    { name: 'LinkedIn', url: 'https://www.linkedin.com/in/jorge-castilla-coello-786713282/', icon: 'assets/images/linkedin.svg', darkIcon: 'assets/images/linkedin.svg' },
+    { name: 'Twitter', url: 'https://twitter.com/JorgeJCCoello', icon: 'assets/images/x.svg', darkIcon: 'assets/images/x-white.svg' }, // Add your Twitter link
+    { name: 'Github', url: 'https://github.com/espartanlast1', icon: 'assets/images/github.svg', darkIcon: 'assets/images/github-white.svg' } // Add your Github link
+];
+
 function buildFooter() {
     // Create footer element
     const footer = document.createElement('footer');
     footer.classList.add('footerF');
-
-    // Define social media links and icons
-    const socialMedias = [
-        { name: 'LinkedIn', url: 'https://www.linkedin.com/in/jorge-castilla-coello-786713282/', icon: 'assets/images/linkedin.svg' },
-        { name: 'Twitter', url: 'https://twitter.com/JorgeJCCoello', icon: 'assets/images/x.svg' }, // Add your Twitter link
-        { name: 'Github', url: 'https://github.com/espartanlast1', icon: 'assets/images/github.svg' } // Add your Github link
-    ];
 
     // Create and append social media links
     const socialMediaFlexbox = document.createElement('div');
@@ -23,8 +24,9 @@ function buildFooter() {
         link.href = media.url;
 
         const img = document.createElement('img');
-        img.src = media.icon;
+        img.src = window.matchMedia('(prefers-color-scheme: dark)').matches ? media.darkIcon : media.icon;
         img.alt = `${media.name}`;
+        img.className = 'social-icon';
 
         link.appendChild(img);
         mediaDiv.appendChild(link);
@@ -39,10 +41,10 @@ function buildFooter() {
 }
 
 function adjustFooterPosition() {
-    var footer = document.querySelector('.footerF');
+    const footer = document.querySelector('.footerF');
     if (footer) { // Check if the footer exists
-        var bodyHeight = document.body.scrollHeight;
-        var viewportHeight = window.innerHeight;
+        const bodyHeight = document.body.scrollHeight;
+        const viewportHeight = window.innerHeight;
 
         if (bodyHeight <= viewportHeight) {
             footer.style.position = 'absolute';
@@ -55,10 +57,24 @@ function adjustFooterPosition() {
     }
 }
 
+// Function to update logos for color scheme changes
+function updateLogosForColorScheme() {
+    document.querySelectorAll('.social-icon').forEach(img => {
+        const media = socialMedias.find(m => m.name.toLowerCase() === img.alt.toLowerCase());
+        if (media) {
+            img.src = window.matchMedia('(prefers-color-scheme: dark)').matches ? media.darkIcon : media.icon;
+        }
+    });
+}
+
+// Listen for changes in color scheme
+window.matchMedia('(prefers-color-scheme: dark)').addListener(updateLogosForColorScheme);
+
 // Ensure the DOM is fully loaded before executing the script
 document.addEventListener('DOMContentLoaded', function() {
     buildFooter();
     adjustFooterPosition();
+    updateLogosForColorScheme();
 });
 
 // Update footer position on resize and scroll
